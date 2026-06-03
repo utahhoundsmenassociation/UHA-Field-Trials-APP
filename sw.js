6737c9eb3adf7a5f33951019ed1cc88399dc1da3
April Thatcher <mccraethatcher@gmail.com>
2026-06-02 20:50:23 -0600

    Fix 404 rename to index

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
 const CACHE_NAME = 'uha-trials-v6_8';
 const FILES_TO_CACHE = [
-  '/uha_field_trials_v6_8.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-// When the app is first installed, cache all the files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -16,7 +15,6 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-// Clean up old cached versions when we update
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keyList => {
@@ -30,15 +28,11 @@ self.addEventListener('activate', event => {
   self.clients.claim();
 });
 
-// When the app tries to load something, check the cache first
-// If we have it cached, use that. If not, try the internet.
 self.addEventListener('fetch', event => {
-  // Always try to get the xlsx library from the internet (it's big and external)
   if (event.request.url.includes('cdnjs.cloudflare.com')) {
     event.respondWith(fetch(event.request));
     return;
   }
-
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
